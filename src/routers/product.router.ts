027f9c33a2c23c87309ae5278cf2a9a80f2efcd6
raphael <you@example.com>
2024-09-05 23:42:34 +0100

    removed middleware from cron

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -9,10 +9,11 @@ import {
 } from "../controllers/product.controllers";
 
 const router = express.Router();
+router.post("/cron", checkAndUpdatePrices);
+
 router.use(authMiddleware);
 router.post("/addProduct", addProduct);
 router.get("/getProduct/:id", getProduct);
 router.get("/getProductsHome", getProductsHome);
 router.delete("/delete", deleteProduct);
-router.post("/cron", checkAndUpdatePrices);
 export default router;
